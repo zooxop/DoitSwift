65a88d2f70193d54f469d6b33e4ddea0ed9e5f42
zooxop <zooxop@gmail.com>
2020-10-06 01:57:21 +0900

    Making MuliPickerview

---
+++ MultiComp_PickerView/MultiComp_PickerView/MultiComp_PickerView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MultiComp_PickerView
+//
+//  Created by 문철현 on 2020/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var pickerImage: UIPickerView!
+    @IBOutlet var lblImageFileName: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
